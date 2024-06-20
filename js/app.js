a8736504b8eceb1d23e56734d8ce30a27442c6c6
EncoderBexruzFratter <sultonovaodina06@gmail.com>
2024-06-20 20:07:26 +0500

    add

--- js/app.js
+++ js/app.js
@@ -5,92 +5,104 @@ const timeline = gsap.timeline();
 timeline.from(".hero__logo", {
   y: 50,
   duration: 1,
-  ease: "ease",
-});
-gsap.to("#hero__left__top__btn_1", {
-  scale: 1,
-  duration: 1,
-  ease: "ease",
-});
-gsap.to(".hero__left__bottom__link__img_box", {
-  scale: 1,
-  duration: 1,
-  ease: "ease",
-});
-gsap.to("#hero__left__top__btn_2", {
-  scale: 1,
-  duration: 1,
-  ease: "ease",
+  ease: "ease"
 });
 
-gsap.to(".hero__left__center__heading", {
+const elementsToScale = [
+  "#hero__left__top__btn_1",
+  ".hero__left__bottom__link__img_box",
+  "#hero__left__top__btn_2",
+  ".hero__left__bottom__link",
+  ".hero__right__bg",
+  ".hero__right_top__btn_1",
+  ".hero__right_top__btn_2",
+  ".hero__right__bottom__download"
+];
+
+elementsToScale.forEach(selector => {
+  gsap.to(selector, {
+    scale: 1,
+    duration: 1
+  });
+});
+
+timeline.to(".hero__left__center__heading", {
   y: 50,
   duration: 1,
-  ease: "ease",
-});
-gsap.to(".hero__left__center__title_top span", {
-  y: -67,
-  duration: 1,
-  ease: "ease",
-});
-gsap.to(".hero__left__center__title_center span", {
-  y: -67,
-  duration: 1,
-  ease: "ease",
+  ease: "ease"
 });
 
-gsap.to(".hero__left__center__title_bottom span", {
-  y: -67,
-  duration: 1,
-  ease: "ease",
+const titleSpans = [
+  ".hero__left__center__title_top span",
+  ".hero__left__center__title_center span",
+  ".hero__left__center__title_bottom span"
+];
+
+titleSpans.forEach(selector => {
+  gsap.to(selector, {
+    y: -67,
+    duration: 1,
+    ease: "ease"
+  });
 });
-gsap.to(".hero__left__bottom__explore__link__text", {
+
+timeline.to(".hero__left__bottom__explore__link__text", {
   x: 30,
   opacity: 1,
   duration: 1,
-  ease: "ease",
+  ease: "ease"
 });
+
 gsap.to(".hero__left__bottom__link__img_bg_black", {
   scale: 1,
-  duration: 0.5,
-});
-gsap.to(".hero__left__bottom__link__img_bg_black", {
-  scale: 1,
-  duration: 0.5,
+  duration: 0.5
 });
 
-gsap.to(".hero__left__bottom__link", {
-  scale: 1,
-  duration: 1,
-});
+const rightTitles = [
+  ".hero__right__top__title_1_left",
+  ".hero__right__top__title_2_left"
+];
 
-gsap.to(".hero__right__bg", {
-  scale: 1,
-  duration: 1,
-});
-
-gsap.to(".hero__right__top__title_1_left", {
-  y: -46,
-  duration: 0.6,
-});
-gsap.to(".hero__right__top__title_2_left", {
-  y: -46,
-  duration: 0.6,
-});
-gsap.to(" .hero", {
-  opacity: 1,
-  duration: 0.6,
-});
-gsap.to(".hero__right_top__btn_1", {
-  scale: 1,
-  duration: 1,
-
-});
-gsap.to(".hero__right_top__btn_2", {
-  scale: 1,
-  duration: 1,
-});
-gsap.to(".hero__right__bottom__download", {
-    scale: 1,
-    duration: 1,
+rightTitles.forEach(selector => {
+  gsap.to(selector, {
+    y: -46,
+    duration: 0.6
   });
+});
+
+gsap.to(".hero", {
+  opacity: 1,
+  duration: 0.6
+});
+
+gsap.to(".payment__bottom", {
+  // maxWidth: 1440,
+  // x:-100,
+  // y:-240,
+  // height:900,
+  scale:2,
+  duration:1,
+  scrollTrigger: {
+    trigger: ".payment__bottom",
+    start: "top bottom",
+    end: "bottom",
+    scrub: true,
+    markers:true
+  }
+});
+
+gsap.to(".hg", {
+  // maxWidth: 1440,
+  // x:-100,
+  // y:-240,
+  // height:900,
+  scale:1.85,
+  duration:1,
+  scrollTrigger: {
+    trigger: ".payment__bottom",
+    start: "bottom -100px",
+    end: "bottom -bottom",
+    scrub: true,
+    markers:true
+  }
+});
