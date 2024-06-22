4630b2336ad77f56cbe1af671cffb6a6cbf96ab7
EncoderBexruzFratter <sultonovaodina06@gmail.com>
2024-06-22 14:13:15 +0500

    add

---
+++ js/app.js
@@ -0,0 +1,127 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//   const report__title_2 = document.querySelector("#report__title_2");
+//   let counterValue2 = 0;
+//   const intervalId2 = setInterval(() => {
+//     counterValue2++;
+//     report__title_2.textContent = counterValue2 + "%";
+
+//     if (counterValue2 >= 98) {
+//       clearInterval(intervalId2);
+//     }
+//   }, 50);
+//   const report__title_3 = document.querySelector("#report__title_3");
+//   let counterValue3 = 0;
+//   const intervalId3 = setInterval(() => {
+//     counterValue3++;
+//     report__title_3.textContent = counterValue3;
+
+//     if (counterValue3 >= 500) {
+//       clearInterval(intervalId3);
+//     }
+//   }, 10);
+
+gsap.to(".gsap__scr", {
+  scrollTrigger: {
+    trigger: ".gsap__scr",
+    start: "-410 410",
+    end: "bottom",
+    toggleClass: "report__title_1",
+    // markers: true,
+    onEnter: () => {
+      if (document.querySelector(".report__title_1")) {
+        countplus();
+      }
+    },
+    onLeaveBack: () => {
+      count_0();
+    },
+  },
+});
+
+// countplus function definition
+function countplus() {
+  const reportTitle = document.querySelector(".report__title_1");
+  if (!reportTitle) return;
+
+  let counterValue = 0;
+  const intervalId = setInterval(() => {
+    counterValue++;
+    reportTitle.textContent = counterValue + "+";
+
+    if (counterValue >= 100) {
+      clearInterval(intervalId);
+    }
+  }, 50);
+}
+function count_0() {
+  reportTitle.textContent = "0";
+}
+
+gsap.to(".gsap__scr1", {
+  scrollTrigger: {
+    trigger: ".gsap__scr1",
+    start: "-570 410",
+    end: "bottom",
+    toggleClass: "report__title_2",
+    // markers: true,
+    onEnter: () => {
+      if (document.querySelector(".report__title_2")) {
+        countplus1();
+      }
+    },
+    onLeaveBack: () => {
+      count_1();
+    },
+  },
+});
+
+// countplus function definition
+function countplus1() {
+  const report__title_2 = document.querySelector(".report__title_2");
+  let counterValue2 = 0;
+  const intervalId2 = setInterval(() => {
+    counterValue2++;
+    report__title_2.textContent = counterValue2 + "%";
+
+    if (counterValue2 >= 98) {
+      clearInterval(intervalId2);
+    }
+  }, 50);
+}
+function count_1() {
+  reportTitle.textContent = "0";
+}
+gsap.to(".gsap__scr2", {
+  scrollTrigger: {
+    trigger: ".gsap__scr2",
+    start: "-730 410",
+    end: "bottom",
+    toggleClass: "report__title_3",
+    // markers: true,
+    onEnter: () => {
+      if (document.querySelector(".report__title_3")) {
+        countplus2();
+      }
+    },
+    onLeaveBack: () => {
+      count_3;
+    },
+  },
+});
+
+function countplus2() {
+  const report__title_3 = document.querySelector(".report__title_3");
+  let counterValue3 = 0;
+  const intervalId3 = setInterval(() => {
+    counterValue3++;
+    report__title_3.textContent = counterValue3;
+
+    if (counterValue3 >= 500) {
+      clearInterval(intervalId3);
+    }
+  }, 10);
+}
+function count_3() {
+  reportTitle.textContent = "0";
+}
--- js/gsap.js
+++ js/gsap.js
@@ -5,7 +5,7 @@ const timeline = gsap.timeline();
 timeline.from(".hero__logo", {
   y: 50,
   duration: 1,
-  ease: "ease"
+  ease: "ease",
 });
 
 const elementsToScale = [
@@ -16,33 +16,39 @@ const elementsToScale = [
   ".hero__right__bg",
   ".hero__right_top__btn_1",
   ".hero__right_top__btn_2",
-  ".hero__right__bottom__download"
+  ".hero__right__bottom__download",
 ];
 
-elementsToScale.forEach(selector => {
+elementsToScale.forEach((selector) => {
   gsap.to(selector, {
     scale: 1,
-    duration: 1
+    duration: 1,
   });
+});
+
+gsap.to(".hero__left__bottom__link", {
+  delay: 1,
+  duration: 1,
+  maxWidth: 196,
 });
 
 timeline.to(".hero__left__center__heading", {
   y: 50,
   duration: 1,
-  ease: "ease"
+  ease: "ease",
 });
 
 const titleSpans = [
   ".hero__left__center__title_top span",
   ".hero__left__center__title_center span",
-  ".hero__left__center__title_bottom span"
+  ".hero__left__center__title_bottom span",
 ];
 
-titleSpans.forEach(selector => {
+titleSpans.forEach((selector) => {
   gsap.to(selector, {
     y: -67,
     duration: 1,
-    ease: "ease"
+    ease: "ease",
   });
 });
 
@@ -50,126 +56,119 @@ timeline.to(".hero__left__bottom__explore__link__text", {
   x: 30,
   opacity: 1,
   duration: 1,
-  ease: "ease"
+  ease: "ease",
 });
 
 gsap.to(".hero__left__bottom__link__img_bg_black", {
   scale: 1,
-  duration: 0.5
+  maxWidth: 196,
+  duration: 0.5,
 });
 
 const rightTitles = [
   ".hero__right__top__title_1_left",
-  ".hero__right__top__title_2_left"
+  ".hero__right__top__title_2_left",
 ];
 
-rightTitles.forEach(selector => {
+rightTitles.forEach((selector) => {
   gsap.to(selector, {
     y: -46,
-    duration: 0.6
+    duration: 0.6,
   });
 });
 
 gsap.to(".hero", {
   opacity: 1,
-  duration: 0.6
-});
-
-gsap.to(".payment__bottom", {
-  // maxWidth: 1440,
-  // x:-100,
-  // y:-240,
-  // height:900,
-  scale:2,
-  duration:1,
-  scrollTrigger: {
-    trigger: ".payment__bottom",
-    start: "top bottom",
-    end: "top 200px",
-    scrub: true,
-  }
-});
-
-gsap.to(".hg", {
-  // maxWidth: 1440,
-  // x:-100,
-  // y:-240,
-  // height:900,
-  scale:1.85,
-  duration:1,
-  scrollTrigger: {
-    trigger: ".hg",
-    start: "bottom 200px",
-    end: "bottom 100",
-    scrub: true,
-    // markers:true,
-  }
-});
-gsap.to(".payment__bg", {
-  // maxWidth: 1440,
-  // x:-100,
-  // y:-240,
-  // height:900,
-  backgroundColor: "#232323",
-  duration:3,
-  scrollTrigger: {
-    trigger: ".payment__bottom",
-    start: "bottom 500px",
-    end: "bottom 500",
-    scrub: true,
-    // markers:true,
-  }
+  duration: 0.6,
 });
 gsap.to(".portfolio__title", {
-  y:-60,
-  duration:1,
+  y: -60,
+  duration: 1,
   scrollTrigger: {
     trigger: ".portfolio__title",
-    start: "top 300",
+    start: "top 500",
     end: "bottom 300",
-    // markers:true
-  }
-})
-gsap.to(".portfolio__btn__box", {
-  x:-330,
-  duration:1,
-  scrollTrigger: {
-    trigger: ".portfolio__title",
-    start: "top 300",
-    end: "bottom 300",
-    // markers:true
-  }
-})
+    // markers:true,
+    scrub: 2,
+  },
+});
+
 gsap.to(".portfolio__card__top", {
-  scale:1,
-  duration:1,
+  scale: 1,
+  duration: 3,
   scrollTrigger: {
     trigger: ".portfolio__title",
-    start: "top 300",
-    end: "bottom 300",
+    start: "top center",
+    end: "bottom center",
     // markers:true
-  }
-})
+    scrub: 4,
+  },
+});
 gsap.to(".portfolio__card__center", {
-  scale:1,
-  duration:3,
+  scale: 1,
+  duration: 3,
   scrollTrigger: {
     trigger: ".portfolio__card__center",
     start: "top center",
     end: "bottom center",
-    // markers:true
-  }
-})
+    // markers:true,
+    scrub: 4,
+  },
+});
 gsap.to(".portfolio__card__bottom", {
-  scale:1,
-  duration:3,
+  scale: 1,
+  duration: 3,
   scrollTrigger: {
     trigger: ".portfolio__card__bottom",
     start: "top center",
     end: "bottom center",
-    // markers:true
-  }
-})
+    // markers:true,
+    scrub: 4,
+  },
+});
 
-
+gsap.to(".payment__scroll__box", {
+  scale: 2.1,
+  duration: 5,
+  scrollTrigger: {
+    trigger: ".payment__scroll__box",
+    start: "-1000 -500",
+    end: "bottom bottom",
+    // markers: true,
+    scrub: 4,
+  },
+});
+gsap.to(".payment__container", {
+  maxWidth:1360,
+  duration: 5,
+  scrollTrigger: {
+    trigger: ".payment__container",
+    start: "600 400",
+    end: "1000 400",
+    markers: true,
+    scrub: 2,
+  },
+});
+gsap.to(".payment", {
+  backgroundColor:"#232323",
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".payment",
+    start: "600 400",
+    end: "1000 400",
+    // markers: true,
+    scrub: true,
+  },
+});
+gsap.to(".portfolio__btn__box", {
+  x: -330,
+  // duration: 1,
+  scrollTrigger: {
+    trigger: ".portfolio__btn__box",
+    start: "top 700",
+    end: "bottom 300",
+    // markers:true,
+    scrub: 4,
+  },
+});
 
