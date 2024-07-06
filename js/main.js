d097faec38c05865de3551e5780cdbd9b65e873e
EncoderBexruzFratter <sultonovaodina06@gmail.com>
2024-07-06 13:50:50 +0500

    add

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ portfolio__btn.forEach(function(item , index){
         portfolio__btn[index].classList.add("portfolio__btn__active")
     })
 })
+
+let asked__right__row = document.querySelectorAll(".asked__right__row")
+let asked__right__column = document.querySelectorAll(".asked__right__column")
+let asked__row__btn = document.querySelectorAll(".asked__row__btn")
+
+asked__right__column.forEach(function(item , index){
+    item.addEventListener("click" , function(){
+        asked__right__row[index].classList.toggle("asked__right__row__active")
+        asked__row__btn[index].classList.toggle("asked__row__btn__active")
+    })
+    console.log([index]);
+})
